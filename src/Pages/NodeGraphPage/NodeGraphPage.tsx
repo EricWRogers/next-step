a5178e35ecc6bbaa24f02a2af5d1d6377679ee15
EricWRogers <ericwilliamrogers@gmail.com>
2024-07-11 17:07:41 -0500

    look at custom nodes

--- src/Pages/NodeGraphPage/NodeGraphPage.tsx
+++ src/Pages/NodeGraphPage/NodeGraphPage.tsx
@@ -7,7 +7,7 @@ import Button from 'beautiful-react-ui/@types/Button';
 
 const DiagramExample: React.FC = () => {
   // define the initial schema
-  const initialSchema: any = {
+  const initialSchema: DiagramSchema<unknown> = {
     nodes: [
       {
         id: 'node-1',
@@ -25,6 +25,25 @@ const DiagramExample: React.FC = () => {
     links: [
       { input: 'port-2', output: 'port-1' },
     ],
+  };
+
+  const CustomNode = (props : any) => {
+    const { inputs } = props;
+
+    console.log(props);
+    
+    return (
+      <div style={{ background: '#717EC3', borderRadius: '10px' }}>
+        <div style={{ padding: '10px', color: 'white'  }}>
+          Custom Node
+        </div>
+        <div style={{marginTop: '20px'}}>
+          {inputs.map((port : any) => React.cloneElement(port, {
+            style: { width: '50px', height: '25px', background: '#1B263B' }
+          }))}
+        </div>
+      </div>
+    );
   };
 
   const [schema, { onChange, addNode, removeNode }] = useSchema(initialSchema);
@@ -45,12 +64,33 @@ const DiagramExample: React.FC = () => {
     };
 
     addNode(nextNode);
+
+    console.log(schema.nodes);
+  };
+
+  const addTrueAndFalseNode = () => {
+    const nextNode: any = {
+      id: `node-${schema.nodes.length + 1}`,
+      content: `Node ${schema.nodes.length + 1}`,
+      coordinates: [
+        schema.nodes[schema.nodes.length - 1].coordinates[0] + 100,
+        schema.nodes[schema.nodes.length - 1].coordinates[1],
+      ],
+      render: CustomNode,
+      inputs: [ { id: `port-${Math.random()}` } ],
+      outputs: [
+        { id: `port-${Math.random()}` },
+        { id: `port-${Math.random()}` },
+      ],
+    };
+
+    addNode(nextNode);
   };
 
   return (
     <div style={{ height: '22.5rem' }}>
       <button color="primary" onClick={() => addNewNode(1, 4)}>Add Multi Choice</button>
-      <button color="primary" onClick={() => addNewNode(1, 2)}>Add True & False</button>
+      <button color="primary" onClick={() => addTrueAndFalseNode()}>Add True & False</button>
       <Diagram schema={schema} onChange={onChange} />
     </div>
   );
